72ca87f7e91471cc01d228f26a393d8a81dd1340
apolomoraes <vlogsapolo@gmail.com>
2026-06-18 15:37:16 -0300

    feat: implementa service e controller de autenticação JWT

--- src/api/controllers/AuthController.js
+++ src/api/controllers/AuthController.js
@@ -1,4 +1,10 @@
+const AuthService = require("../../application/services/AuthService")
 class AuthController {
-  async create(req, res) { }
+  async create(req, res) {
+    const { email, password } = req.body
+    const service = new AuthService()
+    const { usuario, token } = await service.create({ email, password })
+    return res.status(200).json({ usuario, token })
+  }
 }
 module.exports = AuthController
--- src/application/services/AuthService.js
+++ src/application/services/AuthService.js
@@ -1,5 +1,35 @@
+const AppError = require("../../utils/AppError")
+const { compare } = require("bcryptjs")
+const { sign } = require("jsonwebtoken")
+const knex = require("../../infrastructure/database/knex")
+const authConfig = require("../../configs/auth")
+
 class AuthService {
-  async create() { }
+  async create({ email, password }) {
+    if (!email || !password) {
+      throw new AppError("Preencha todos os campos")
+    }
+
+    const usuario = await knex("usuarios").where({ email }).first()
+    if (!usuario) {
+      throw new AppError("Email e/ou senha incorreta", 401)
+    }
+
+    const senhaCorreta = await compare(password, usuario.password)
+    if (!senhaCorreta) {
+      throw new AppError("Email e/ou senha incorreta", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({ role: usuario.role }, secret, {
+      subject: String(usuario.id),
+      expiresIn
+    })
+
+    const { password: _, ...usuarioSemSenha } = usuario
+
+    return { usuario: usuarioSemSenha, token }
+  }
 }
 
-module.exports = new AuthService();
+module.exports = AuthService
